6072c15e18b665a7c1e89311c2476fa6d9e02447
Eduardo Lozano Munera <eduardo.lozano@hutchgames.com>
2017-10-18 12:09:57 +0100

    toolbar buttons

---
+++ Assets/Scripts/ToolbarButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolbarButton : MonoBehaviour
+{
+	public Image Border;
+	public Image Icon;
+	public Text Text;
+
+
+	public KeyCode AssignedKey = KeyCode.Alpha1;
+}
